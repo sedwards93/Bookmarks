68ebefde758129a1e5b77502c5ee65eec988538a
Symion Edwards <symion.edwards@gmail.com>
2020-11-19 12:30:20 +0000

    Implemented functionality tor return SQL data as a ruby object

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,24 +1,34 @@
 require 'pg'
 
 class Bookmark
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark['url']}
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end
   end
 
-  def self.create(url:)
+  def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
   
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
   end
 end
 
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,22 +5,25 @@ describe Bookmark do
     it 'returns all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-      
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+   
       bookmarks = Bookmark.all
-
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+   
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      # expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
 
     describe '.create' do 
       it 'creates a new bookmark' do 
-        Bookmark.create(url: 'http://www.testbookmark.com')
+        bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
 
-        expect(Bookmark.all).to include 'http://www.testbookmark.com'
+        expect(bookmark['url']).to eq 'http://www.testbookmark.com'
+        expect(bookmark['title']).to eq 'Test Bookmark'
       end
     end 
   end
