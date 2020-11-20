49b9cb332f9029c6023bdb77e2dbbf0f93c371df
Symion Edwards <symion.edwards@gmail.com>
2020-11-20 09:18:15 +0000

    Added a Bookmark.find method so that user can see content of the bookmark they're editing

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -18,39 +18,52 @@ describe Bookmark do
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
+  end
 
-    describe '.create' do 
-      it 'creates a new bookmark' do 
-        bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
-        persisted_data = persisted_data(id: bookmark.id)
-    
-        expect(bookmark).to be_a Bookmark
-        expect(bookmark.id).to eq persisted_data['id']
-        expect(bookmark.title).to eq 'Test Bookmark'
-        expect(bookmark.url).to eq 'http://www.testbookmark.com'
-      end
-    end 
-
-    describe '.delete' do
-      it 'deletes a bookmark' do
-        bookmark = Bookmark.create(title: "Makers Academy", url: 'http://www.makersacademy.com')
-
-        Bookmark.delete(id: bookmark.id)
-
-        expect(Bookmark.all.length).to eq 0
-      end
+  describe '.create' do 
+    it 'creates a new bookmark' do 
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      persisted_data = persisted_data(id: bookmark.id)
+  
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
+  end 
 
-    describe '.update' do
-      it 'updates bookmarks' do
-        bookmark = Bookmark.create(title: "Makers Academy", url: 'http://www.makersacademy.com') 
-        updated_bookmark = Bookmark.update(id: bookmark.id, title: "Snakers Academy", url: 'http://www.snakersacademy.com')
-        
-        expect(updated_bookmark).to be_a Bookmark
-        expect(updated_bookmark.id).to eq(bookmark.id)
-        expect(updated_bookmark.title).to eq "Snakers Academy"
-        expect(updated_bookmark.url).to eq 'http://www.snakersacademy.com'
-      end
+  describe '.delete' do
+    it 'deletes a bookmark' do
+      bookmark = Bookmark.create(title: "Makers Academy", url: 'http://www.makersacademy.com')
+
+      Bookmark.delete(id: bookmark.id)
+
+      expect(Bookmark.all.length).to eq 0
+    end
+  end
+
+  describe '.update' do
+    it 'updates bookmarks' do
+      bookmark = Bookmark.create(title: "Makers Academy", url: 'http://www.makersacademy.com') 
+      updated_bookmark = Bookmark.update(id: bookmark.id, title: "Snakers Academy", url: 'http://www.snakersacademy.com')
+      
+      expect(updated_bookmark).to be_a Bookmark
+      expect(updated_bookmark.id).to eq(bookmark.id)
+      expect(updated_bookmark.title).to eq "Snakers Academy"
+      expect(updated_bookmark.url).to eq 'http://www.snakersacademy.com'
+    end
+  end
+  
+  describe '.find' do
+    it 'returns the requested bookmark object' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+
+      result = Bookmark.find(id: bookmark.id)
+
+      expect(result).to be_a Bookmark
+      expect(result.id).to eq bookmark.id
+      expect(result.title).to eq 'Makers Academy'
+      expect(result.url).to eq 'http://www.makersacademy.com'
     end
   end
 end
